a9cc61547bc05a485aa917ca98138e906a1722a7
jeison0894 <jeisson0894@gmail.com>
2021-11-09 19:44:06 -0500

    Header&Footer with Responsive Add

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import Footer from "./components/views/projects/Footer.jsx";
+import Header from "./components/views/projects/Header.jsx";
 
 const App = () => {
   return (
-    <div>
-      si f
-    </div>
-  )
-}
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+};
 
-export default App
+export default App;
--- client/src/components/views/projects/projects.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function projects() {
-  return (
-    <div>
-      <h1>Prueba push </h1>
-    </div>
-  );
-}
