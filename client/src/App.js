f4f5a6b7723a004b22706987103648e77007331e
Jaiber Flor <jaiver.navia@gmail.com>
2021-11-18 11:53:34 -0500

    ruta con router

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
 import Footer from "./components/views/projects/Footer.jsx";
 import Header from "./components/views/projects/Header.jsx";
+import  Announce from "./components/views/viewannounce/ViewAnnounce";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom"; 
+
 
 const App = () => {
   return (
     <>
       <Header />
       <Footer />
+
+      <Router>
+        <Routes>
+        <Route path='/anuncio' element={Announce} />
+        </Routes>
+      </Router>
     </>
   );
 };
